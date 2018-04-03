b33f21decf72e88628a11724a43b85fc7d38dae8
sidharthchugh <chughsidharth@hotmail.com>
2018-04-03 18:40:56 +0200

    Added partial and refactor things

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    before_action :set_articles, only: [:edit, :update, :show, :destroy] # common method called before anything else
 
     def index
         @articles = Article.all # get all articles
@@ -8,8 +9,7 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
-    def edit 
-        @article = Article.find(params[:id])
+    def edit
     end
 
     def create
@@ -24,11 +24,9 @@ class ArticlesController < ApplicationController
     end
 
     def show
-        @article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice] = "Article was sucessfly updated"
             redirect_to article_path(@article)
@@ -38,13 +36,15 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         flash[:notice] = "Articles was successfully deleted"
         redirect_to articles_path
     end
 
     private
+        def set_articles
+            @article = Article.find(params[:id])
+        end
         def article_params
             params.require(:article).permit(:title, :description)
         end 
